20fad3778354abdb807639a2a58b1bd65142e370
William Dobson <wjedobson@gmail.com>
2022-02-13 23:34:52 +0200

    updated style

--- assets/js/betrayalDice.js
+++ assets/js/betrayalDice.js
@@ -1,16 +1,16 @@
  function rollDice() {
 	var showDice = document.getElementById("showDie");
-	
+
 	while (showDice.hasChildNodes()) {
 		showDice.removeChild(showDice.lastChild);
 	}
-	
-	var playerDice = document.getElementById("playerDice").value; 
-	
-	var oppDice = document.getElementById("oppDice").value; 
-	
-	var status = document.getElementById("status"); 
-	
+
+	var playerDice = document.getElementById("playerDice").value;
+
+	var oppDice = document.getElementById("oppDice").value;
+
+	var status = document.getElementById("status");
+
 	var playerRoll = new Array();
 	for (var i = 1; i <= playerDice; i++){
 		playerRoll.push(Math.floor(Math.random() * 3));
@@ -20,19 +20,19 @@
 	for (var j = 1; j <= oppDice; j++){
 		oppRoll.push(Math.floor(Math.random() * 3));
 	}
-	
+
 	for (var c in playerRoll) {
-		var diceShow = document.createElement('div');
-		diceShow.id = "die"+[c]; 
+		var diceShow = document.createElement('span');
+		diceShow.id = "die"+[c];
 		diceShow.className = "dice";
 		diceShow.innerHTML = playerRoll[c];
 		showDice.appendChild(diceShow);
 	}
 
 	for (var d in oppRoll) {
-		var diceShowOpp = document.createElement('div');
-		diceShowOpp.id = "die"; 
-		diceShowOpp.className = "oppDice";
+		var diceShowOpp = document.createElement('span');
+		diceShowOpp.id = "die";
+		diceShowOpp.className = "dice oppDice";
 		diceShowOpp.innerHTML = oppRoll[d];
 		showDice.appendChild(diceShowOpp);
 	}
@@ -54,10 +54,10 @@
 	} else if(playerDiceTotal < oppDiceTotal) {
 		status.innerHTML += "<br>You lost the dice roll!!";
 	}
-	
-	
+
+
 }
-			
+
 function refresh() {
 	window.location.reload();
 }
