40c12c126538a986d758edf869a15be02c2fa06e
William Dobson <wjedobson@gmail.com>
2022-02-13 22:56:52 +0200

    added files

---
+++ js/betrayalDice.js
@@ -0,0 +1,63 @@
+ function rollDice() {
+	var showDice = document.getElementById("showDie");
+	
+	while (showDice.hasChildNodes()) {
+		showDice.removeChild(showDice.lastChild);
+	}
+	
+	var playerDice = document.getElementById("playerDice").value; 
+	
+	var oppDice = document.getElementById("oppDice").value; 
+	
+	var status = document.getElementById("status"); 
+	
+	var playerRoll = new Array();
+	for (var i = 1; i <= playerDice; i++){
+		playerRoll.push(Math.floor(Math.random() * 3));
+	}
+
+	var oppRoll = new Array();
+	for (var j = 1; j <= oppDice; j++){
+		oppRoll.push(Math.floor(Math.random() * 3));
+	}
+	
+	for (var c in playerRoll) {
+		var diceShow = document.createElement('div');
+		diceShow.id = "die"+[c]; 
+		diceShow.className = "dice";
+		diceShow.innerHTML = playerRoll[c];
+		showDice.appendChild(diceShow);
+	}
+
+	for (var d in oppRoll) {
+		var diceShowOpp = document.createElement('div');
+		diceShowOpp.id = "die"; 
+		diceShowOpp.className = "oppDice";
+		diceShowOpp.innerHTML = oppRoll[d];
+		showDice.appendChild(diceShowOpp);
+	}
+
+	var playerDiceTotal = playerRoll.reduce(addp,0);
+	function addp(a, b) {
+		return a + b;
+	}
+
+	var oppDiceTotal = oppRoll.reduce(addo,0);
+	function addo(a, b) {
+		return a + b;
+	}
+
+	status.innerHTML = "You rolled "+playerDiceTotal+".<br>Your opponent rolled "+oppDiceTotal+".";
+
+	if(playerDiceTotal > oppDiceTotal) {
+		status.innerHTML += "<br>You won the dice roll!!";
+	} else if(playerDiceTotal < oppDiceTotal) {
+		status.innerHTML += "<br>You lost the dice roll!!";
+	}
+	
+	
+}
+			
+function refresh() {
+	window.location.reload();
+}
